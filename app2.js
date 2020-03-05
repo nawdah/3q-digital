134046c2f2e382e60a27ae19616baa9e762cbc68
nawdah <nadaibrhm96@gmail.com>
2020-03-05 13:08:55 -0500

    challenge_2 completed

--- app2.js
+++ app2.js
@@ -50,12 +50,43 @@
 const https = require('https');
 
 const url = encodeURIComponent('https://curiositystream.com/');
-const options = {
-  hostname: 'api.proxycrawl.com',
-  path: '/?token=rvLNYo7Vu1XmaIteRTVE9w&css_click_selector=%23styles__signUpButtonPromoPage___3H-U-&page_wait=1000&url=' + url
+
+function challenge_2(url){
+
+    button1 = '&css_click_selector=%23styles__signUpButtonPromoPage___3H-U-'
+    button2 = '&css_click_selector=%23styles__tryFreeButton___1r0YY'
+    
+    const options = {
+        hostname: 'api.proxycrawl.com',
+        path: '/?token=rvLNYo7Vu1XmaIteRTVE9w&page_wait=1000&url=' + url
+    };
+
+    // https.request(options, (response) => {
+    //     let body = '';
+    //     response.on('data', chunk => body += chunk).on('end', () => console.log(body));
+    // }).end();
+
+    if(true){
+        const b1 = {
+            hostname: 'api.proxycrawl.com',
+            path: '/?token=rvLNYo7Vu1XmaIteRTVE9w' + button1 + '&page_wait=1000&url=' + url
+        };
+
+        const b2 = {
+            hostname: 'api.proxycrawl.com',
+            path: '/?token=rvLNYo7Vu1XmaIteRTVE9w' + button2 + '&page_wait=1000&url=' + url
+        };
+
+        https.request(b1, (response) => {
+            let body = '';
+            response.on('data', chunk => body += chunk).on('end', () => {return console.log('Button 1 has been clicked!')});
+        }).end();
+
+        https.request(b2, (response) => {
+            let body = '';
+            response.on('data', chunk => body += chunk).on('end', () => {return console.log('Button 2 has been clicked!')});
+        }).end();
+    };
 };
 
-https.request(options, (response) => {
-  let body = '';
-  response.on('data', chunk => body += chunk).on('end', () => console.log('Button 1 has been clicked!'));
-}).end();
+challenge_2(url);
