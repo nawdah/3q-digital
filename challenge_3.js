f082f84ded5de54fbb513dbc0af9ea47f8c4d5b6
nawdah <nadaibrhm96@gmail.com>
2020-03-06 13:23:35 -0500

    changes to challenge 3

--- challenge_3.js
+++ challenge_3.js
@@ -1,6 +1,19 @@
 function checkCookies(){
+
+    var elements = [];
+
+    var top_menu = document.getElementsByClassName(".nav-primary").childNodes;
+
+    // Loop through the children of the <ol>
+    for(i=0;i<top_menu.length;i++) {
+    // Test that the node is indeed an <li>
+    if(top_menu[i].nodeName=='li') {
+        // Change this line to manipulate the text however you need
+        elements.push(top_menu[i].className);
+        };
+    };
     
-    var elements = ['.nav-1', '.nav-2', '.nav-3', '.nav-4', '.nav-5', '.nav-6', '.nav-7', '.nav-8', '.nav-9', '.nav-10', '.level0'];
+    // var elements = ['.nav-1', '.nav-2', '.nav-3', '.nav-4', '.nav-5', '.nav-6', '.nav-7', '.nav-8', '.nav-9', '.nav-10', '.level0'];
     
     for(i=0; elements.length; i++){
 
@@ -18,33 +31,3 @@ function checkCookies(){
     }
     return (browserCooker || browserClick)
 };
-    
-//     var nike = document.querySelector('nav-1').addEventListener("click", function(){
-
-//     })
-//     var jordan = 
-//     var adidas = 
-//     var yeezy = 
-//     var shoes = 
-//     var women =
-//     var streetwear =
-//     var trophycase =
-//     var stadiumgoods = 
-//     var gifts =
-//     var journal =
-
-// }
-
-
-    // var elements = [];
-
-    // var top_menu = document.getElementsByClassName("nav-primary").childNodes;
-
-    // // Loop through the children of the <ol>
-    // for(i=0;i<top_menu.length;i++) {
-    // // Test that the node is indeed an <li>
-    // if(top_menu[i].nodeName=='LI') {
-    //     // Change this line to manipulate the text however you need
-    //     elements.push(top_menu[i].className);
-    //     };
-    // };
